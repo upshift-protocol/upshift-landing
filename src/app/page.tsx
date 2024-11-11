d2b51e705c261d46978177c1c975e008f724a4b5
angello <angello@fractalprotocol.org>
2024-11-11 11:15:01 -0500

    clickjacking prevention and banner next to footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,10 @@
 import { Container } from "@mui/material";
 import HeroView from "@/views/hero";
-import BannerView from "@/views/banner";
-import augustSdk from "@/config/august-sdk";
-import { IToken } from "@/utils/types";
 
 export default async function Home() {
-  const pools = await augustSdk.pools.getPools();
-  const tokens: IToken[] = await Promise.all(
-    pools?.map(async (p) => {
-      const price = await augustSdk.getPrice(
-        p.underlying?.symbol?.toLowerCase(),
-      );
-      return {
-        ...p.underlying,
-        price,
-      };
-    }),
-  );
-
   return (
     <Container maxWidth="xl">
       <HeroView />
-      <BannerView pools={pools} tokens={tokens} />
     </Container>
   );
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,10 +3,31 @@ import Typography from "@mui/material/Typography";
 import buildMetadata from "@/config/metadata";
 import Stack from "@mui/material/Stack";
 import { STYLE_VARS } from "@/utils/constants";
+import BannerView from "@/views/banner";
+import augustSdk from "@/config/august-sdk";
+import { IToken } from "@/utils/types";
+import { Container } from "@mui/material";
 
-export default function Footer() {
+export default async function Footer() {
+  const pools = await augustSdk.pools.getPools();
+  const tokens: IToken[] = await Promise.all(
+    pools?.map(async (p) => {
+      const price = await augustSdk.getPrice(
+        p.underlying?.symbol?.toLowerCase(),
+      );
+      return {
+        ...p.underlying,
+        price,
+      };
+    }),
+  );
+
   return (
     <footer>
+      <Container maxWidth="xl">
+        <BannerView pools={pools} tokens={tokens} />
+      </Container>
+
       <Stack
         paddingY={2}
         paddingX={{ xs: "16px", sm: "24px" }}
