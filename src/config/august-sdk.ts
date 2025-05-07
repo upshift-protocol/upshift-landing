4ebfaa88de119587f639244a20401a559d3e3f0f
angello <angello@fractalprotocol.org>
2025-05-07 17:44:39 -0400

    august-sdk

--- src/config/august-sdk.ts
+++ src/config/august-sdk.ts
@@ -7,6 +7,8 @@ const augustSdk = new AugustDigitalSDK({
     43114: `https://avax-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
     1: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
     8453: `https://base-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+    999: `https://autumn-fabled-shard.hype-mainnet.quiknode.pro/0c3002cf3654fbd7136be3106ab88b23e24c7375/evm`,
+    56: `https://bnb-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
   },
   env: process.env.NEXT_PUBLIC_DEV ? "DEV" : "PROD",
 });
